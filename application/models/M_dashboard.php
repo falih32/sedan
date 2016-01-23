0bb11c3cf32291912f02ce1ab9e9abbedf9e528c
Ganteng Imut <Ganteng Imut@169.254.66.227>
2016-01-23 17:33:00 +0700

    Merge origin/master
    Conflicts:
    	sedan.sql

--- application/models/M_dashboard.php
+++ application/models/M_dashboard.php
@@ -15,7 +15,11 @@ class M_dashboard extends CI_Model{
     }
     function TotalPengadaanPerBulan($tahun){
         $this->db->query("SET lc_time_names = 'id_ID'");
-        $data = $this->db->query("SELECT MONTHname(pgd1.pgd_tanggal_input) as bulan, (SELECT COUNT(*) as 'total' FROM t_pengadaan a WHERE YEAR(a.pgd_tanggal_input)=".$tahun." and a.pgd_tipe_pengadaan=0 and MONTHname(a.pgd_tanggal_input) = bulan) as tot_pgd_barang, (SELECT COUNT(*) as 'total' FROM t_pengadaan b WHERE YEAR(b.pgd_tanggal_input)=".$tahun." and b.pgd_tipe_pengadaan=1 and MONTHname(b.pgd_tanggal_input) =  bulan) as tot_pgd_jasa, (SELECT COUNT(*) as 'total' FROM t_pengadaan c WHERE YEAR(c.pgd_tanggal_input)=".$tahun." and c.pgd_tipe_pengadaan=2 and MONTHname(c.pgd_tanggal_input) = bulan) as tot_pgd_konsultan FROM t_pengadaan pgd1 group by bulan order by MONTH(pgd1.pgd_tanggal_input) ")->result();
+        $data = $this->db->query("SELECT MONTHname(pgd1.pgd_tanggal_input) as bulan, "
+                . "(SELECT COUNT(*) as 'total' FROM t_pengadaan a WHERE a.pgd_deleted=0 and YEAR(a.pgd_tanggal_input)=".$tahun." and a.pgd_tipe_pengadaan=0 and MONTHname(a.pgd_tanggal_input) = bulan) as tot_pgd_barang, "
+                . "(SELECT COUNT(*) as 'total' FROM t_pengadaan b WHERE b.pgd_deleted=0 and YEAR(b.pgd_tanggal_input)=".$tahun." and b.pgd_tipe_pengadaan=1 and MONTHname(b.pgd_tanggal_input) =  bulan) as tot_pgd_jasa, "
+                . "(SELECT COUNT(*) as 'total' FROM t_pengadaan c WHERE c.pgd_deleted=0 and YEAR(c.pgd_tanggal_input)=".$tahun." and c.pgd_tipe_pengadaan=2 and MONTHname(c.pgd_tanggal_input) = bulan) as tot_pgd_konsultan "
+                . "FROM t_pengadaan pgd1 WHERE YEAR(pgd1.pgd_tanggal_input)=".$tahun." group by bulan order by MONTH(pgd1.pgd_tanggal_input) ")->result();
        return $data;
     }
 }
